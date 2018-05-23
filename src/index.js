3d15436096e3fa1b1049d77cf2fa6c2257017e1a
patriciamont <fiorellamonteverde@gmail.com>
2018-05-23 10:26:21 -0500

    subiendo nuevos cambios desde casa

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const texto = document.getElementById("mensaje");
+const boton = document.getElementById("botone");
+boton.addEventListener("click",cifrarMensaje);
+
+
+function cifrarMensaje(){
+
+    const text = texto.value;
+}
+
