13af7e7f3c23b4a987fb21bf37bb5a8c9a417604
patriciamont <fiorellamonteverde@gmail.com>
2018-05-23 18:39:05 -0500

    actualizando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-const texto = document.getElementById("mensaje");
+const box_one = document.getElementById("mensaje");
 const boton = document.getElementById("botone");
 boton.addEventListener("click",cifrarMensaje);
+
+const box_two = document.getElementById("descifrar")
+const boton2 = document.getElementById("botone2")
+boton2.addEventListener("click",decifrarMensaje)
+
 
 
 function cifrarMensaje(){
 
-    const text = texto.value;
+    const text = box_one.value;
+    box_two.value = text;
+}
+function decifrarMensaje(){
+    const text = box_one.value;
+    box_two.value = text;
+
 }
 
