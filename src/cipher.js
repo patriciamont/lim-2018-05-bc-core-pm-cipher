69c00e5f85885dee4bf2b93ca3a0a223a2f13ee0
patriciamont <fiorellamonteverde@gmail.com>
2018-05-29 16:00:32 -0500

    subiendo codigo de desplazamiento

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,15 @@
 window.cipher = {
     encode:function (){
+        let d=parseInt(desplazamiento.value);
         let mensaje="";
         let letra=box_one.value;
         for (let i=0;i<letra.length;i++){
-
             let code=letra.charCodeAt(i);
-
             if (code >=65 && code<=90){
-            word_mayuscula= String.fromCharCode((code - 65+33)%26+65);
+            word_mayuscula= String.fromCharCode((code - 65+d)%26+65);
             mensaje +=word_mayuscula;
             }   else if(code >=97 && code<=122){
-             word_minuscula=String.fromCharCode((code - 97+33)%26+97);
+             word_minuscula=String.fromCharCode((code - 97+d)%26+97);
             mensaje+=word_minuscula;
             //si no cumple mis condiciones ejemplo -; cualquier otro signo.
          }else {
@@ -21,8 +20,8 @@ window.cipher = {
     box_two.value=mensaje;
     },
 
-
     decode:function (){
+        let d=parseInt(desplazamiento.value);
         let mensaje="";
         let letra=box_one.value;
         for (let i=0;i<letra.length;i++){
@@ -30,10 +29,13 @@ window.cipher = {
             let code=letra.charCodeAt(i);
 
             if (code >=65 && code<=90){
-            word_mayuscula= String.fromCharCode((code - 65+19)%26+65);
+            word_mayuscula= String.fromCharCode((code - 65-d)%26+91);
             mensaje +=word_mayuscula;
             }   else if(code >=97 && code<=122){
-             word_minuscula=String.fromCharCode((code - 97+19)%26+97);
+             word_minuscula=String.fromCharCode((code - 97-d)%26+123);
+                    if((code-97-d)%26==0 ||(code-97-d)%26>0){
+                        word_minuscula=String.fromCharCode((code - 97-d)%26+97);
+                    }
             mensaje+=word_minuscula;
          //si no cumple mis condiciones ejemplo -; cualquier otro signo.
             }    else {
@@ -43,9 +45,12 @@ window.cipher = {
     }
     box_two.value=mensaje;
     },
+
     createCipherWithOffset:function(){
         const letra= "abcdefghijklmnopqrstuvwxyz";
         const metod= cipher.encode(letra);
         const metod2= cipher.decode(letra);
+
     }
-};
+}
+  
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ boton.addEventListener("click",cipher.encode);
 
 let box_two = document.getElementById("descifrar");
 
-
-
 let boton2= document.getElementById("botone2");
 boton2.addEventListener("click",cipher.decode);
+let desplazamiento=document.getElementById("desplazar");
+
